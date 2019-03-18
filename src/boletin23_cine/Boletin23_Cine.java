ebbf8ba54fe554e43fb9f6a988091064611296d5
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2019-03-18 09:34:37 +0100

    2.finish

--- src/boletin23_cine/Boletin23_Cine.java
+++ src/boletin23_cine/Boletin23_Cine.java
@@ -16,7 +16,32 @@ public class Boletin23_Cine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+                int personas = 0;
+        do {
+            try {
+                personas = Metodos.pedirPersonas();
+            } catch (FueraDeRangoException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (personas < 1);
+
+        int[] edades = Metodos.randomIntsArray(personas, 5, 60);
+        LinkedList<Integer> entradas = new LinkedList<>();
+
+        float caja = 0;
+        for (int i = 0; i < edades.length; i++) {
+            entradas.add(edades[i]);
+            if (entradas.get(i) >= 5 && entradas.get(i) <= 10) {
+                caja += 1f;
+            } else if (entradas.get(i) <= 17) {
+                caja += 2.5f;
+            } else if (entradas.get(i) > 17) {
+                caja += 3.5f;
+            }
+        }
+        System.out.printf("%.2f € de caja", caja);
+
+    }
     }
 
 }
---
+++ src/boletin23_cine/FueraDeRangoException.java
@@ -0,0 +1,16 @@
+
+package boletin23_cine;
+
+/**
+ *
+ * @author lvazquezdorna
+ */
+
+public class FueraDeRangoException extends Exception {
+    public FueraDeRangoException(){
+        super();
+    }
+    public FueraDeRangoException(String msg){
+        super(msg);
+    }
+}
